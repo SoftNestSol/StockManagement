112c9d2c45fe9c56fe769f8c94c296c337900411
ANDREI TUDOR IORDACHE <andrei-tudor.iordache@s.unibuc.ro>
2023-12-12 01:21:11 +0200

    dbcontext.#

---
+++ StockManagement.Server/ContextModels/StockContext.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+
+
+namespace StockManagement.Server.ContextModels
+
+
+{
+    public class StockContext:DbContext
+    {
+        public StockContext(DbContextOptions<StockContext> options): base(options)
+        {
+        }
+
+        public DbSet<Produs> Produse { get; set; }
+        public DbSet<ProdusInStoc> ProduseInStoc { get; set; }
+        public DbSet<Stoc> Stocuri { get; set; }
+        public DbSet<ProdusInComanda> ProduseInComanda { get; set; }
+        public DbSet<Comanda> Comenzi { get; set; }
+        public DbSet<Furnizor> Furnizori { get; set; }
+        public DbSet<Angajat> Angajati { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<Produs>().HasKey(p => p.ProdusId);
+            modelBuilder.Entity<ProdusInStoc>().HasKey(p => new { p.StocId, p.ProdusId });
+            modelBuilder.Entity<Stoc>().HasKey(s => s.StocId);
+            modelBuilder.Entity<ProdusInComanda>().HasKey(p => new { p.ComandaId, p.ProdusId });
+            modelBuilder.Entity<Comanda>().HasKey(c => c.ComandaId);
+            modelBuilder.Entity<Furnizor>().HasKey(f => f.FurnizorId);
+            modelBuilder.Entity<Angajat>().HasKey(a => a.AngajatId);
+
+           
+            modelBuilder.Entity<ProdusInStoc>()
+                .HasOne(pis => pis.Produs)
+                .WithMany() 
+                .HasForeignKey(pis => pis.ProdusId);
+
+            modelBuilder.Entity<ProdusInStoc>()
+                .HasOne(pis => pis.Stoc)
+                .WithMany() 
+                .HasForeignKey(pis => pis.StocId);
+
+            modelBuilder.Entity<ProdusInComanda>()
+                .HasOne(pic => pic.Produs)
+                .WithMany() 
+                .HasForeignKey(pic => pic.ProdusId);
+
+            modelBuilder.Entity<ProdusInComanda>()
+                .HasOne(pic => pic.Comanda)
+                .WithMany()
+                .HasForeignKey(pic => pic.ComandaId);
+
+            modelBuilder.Entity<Comanda>()
+                .HasOne(c => c.Furnizor)
+                .WithMany() 
+                .HasForeignKey(c => c.FurnizorId);
+
+            modelBuilder.Entity<Comanda>()
+                .HasOne(c => c.Angajat)
+                .WithMany() 
+                .HasForeignKey(c => c.AngajatId);
+
+           
+        }
+    }
+
+}
+}
--- StockManagement.Server/WeatherForecast.cs
+++ StockManagement.Server/WeatherForecast.cs
@@ -6,6 +6,8 @@ namespace StockManagement.Server
 
         public int TemperatureC { get; set; }
 
+        public int TemperatureK { get; set; }
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
